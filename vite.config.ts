446d04a2af1aa48d172bd8e90901b1bc5255736e
Matthias Wilhelm <ankertal@gmail.com>
2025-05-30 22:56:52 +0200

    fix: Update base URL in Vite configuration for GitHub Pages deployment
    - Changed the base URL from '/vegas-github/' to '/git-vegas/' to reflect the correct GitHub repository name for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vegas-github/', // Der Name des GitHub-Repository für GitHub Pages
+  base: '/git-vegas/', // Der Name des GitHub-Repository für GitHub Pages
 })
