cfa2a03b3006c37b91db5189031d69f9c3b76b90
Matthias Wilhelm <ankertal@gmail.com>
2025-06-04 08:27:34 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/git-vegas/sw.js', { scope: '/git-vegas/' })})}
